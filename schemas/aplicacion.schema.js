4999843fe15e007435d20efcfc56f2a43c53f85b
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-11-03 16:08:38 -0600

    rutas /auth añadido

--- schemas/aplicacion.schema.js
+++ schemas/aplicacion.schema.js
@@ -1,18 +1,18 @@
 const Joi = require('joi');
 
 const IdAplicacion = Joi.number().integer();
-const nombre = Joi.string();
+const name = Joi.string();
 const estatus = Joi.number().integer();
 const path = Joi.string();
 
 const createAplicacionSchema = Joi.object({
-  nombre: nombre.required(),
+  name: name.required(),
   estatus: estatus.required(),
   path: path.required(),
 });
 
 const updateAplicacionSchema = Joi.object({
-  nombre,
+  name,
   estatus,
   path
 });
