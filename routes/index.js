9060c963f9a367b180f14b95b9a674a5f1440297
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-11-03 15:24:54 -0600

    rutas agregadas

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const categoriesRouter = require('./categories.routes');
 const usersRouter = require('./users.routes');
 const orderRouter = require('./orders.routes');
 const customersRouter = require('./customers.routes');
+const aplicacionRouter = require('./aplicacion.routes');
+const permisorolRouter = require('./permiso-rol.routes');
+const permisoRouter = require('./permiso.routes');
+const rolusuarioRouter = require('./rol-usuario.routes');
+const rolRouter = require('./rol.routes');
+const usuarioRouter = require('./usuarios.routes');
 
 function routerApi(app) {
   const router = express.Router();
@@ -14,6 +20,12 @@ function routerApi(app) {
   router.use('/users', usersRouter);
   router.use('/orders', orderRouter);
   router.use('/customers', customersRouter);
+  router.use('/aplicacion', aplicacionRouter);
+  router.use('/permiso-rol', permisorolRouter);
+  router.use('/permiso', permisoRouter);
+  router.use('/rol-usuario', rolusuarioRouter);
+  router.use('/rol', rolRouter);
+  router.use('/usuario', usuarioRouter);
 }
 
 module.exports = routerApi;
