0884640d9f1e483b135376456faee0240b820181
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-11-29 12:48:11 -0600

    Añadiendo errores para token, se añidio libreria boom

--- middlewares/validarJwt.js
+++ middlewares/validarJwt.js
@@ -1,48 +1,36 @@
 const { response, request } = require('express');
 const jwt = require('jsonwebtoken');
-const { JWT }= require('../config/config');
+const { JWT } = require('../config/config');
 const UsuarioService = require('../services/usuario.service');
 const service = new UsuarioService();
+const Boom = require('@hapi/boom');
 
+const validarJWT = async (req = request, res = response, next) => {
 
-const validarJWT = async( req = request, res = response, next ) => {
-
-    
-    if ( !req.headers.authorization ) {
-        return res.status(401).json({
-            msg: 'No hay token en la petición'
-        });
+    if (!req.headers.authorization) {
+        return res.status(401).send(Boom.badData(error));
     }
-    
+
     let token = req.headers.authorization.split(" ")[1];
     try {
-        
-        const { credencial } = jwt.verify( token, JWT.secret,{maxAge: JWT.expires} );
 
-        // leer el usuario que corresponde al uid
-        const usuario = await service.findOne( credencial );
+        const { credencial } = jwt.verify(token, JWT.secret, { maxAge: JWT.expires });
+        const usuario = await service.findOne(credencial);
 
-        if( !usuario ) {
+        if (!usuario) {
             return res.status(401).json({
                 msg: 'Token no válido - usuario no existe DB'
             })
         }
-       
+
         req.usuario = usuario;
         next();
 
     } catch (error) {
-
-        console.log(error);
-        res.status(401).json({
-            msg: 'Token no válido'
-        })
+        return res.status(401).send(Boom.badData(error));
     }
 
 }
-
-
-
 
 module.exports = {
     validarJWT
