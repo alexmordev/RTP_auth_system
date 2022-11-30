3538321dc51d1a69ef71e52f2ccde7b609c3368e
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-11-29 18:23:01 -0600

    respaldo a seguir

--- services/permisoRol.service.js
+++ services/permisoRol.service.js
@@ -34,6 +34,7 @@ class PermisoRolService {
 
   async credencialAplicacion(query) {
     const permisoUsuario = await models.Rol.findAll({
+      // order:['idRol'],
       include: [
         {
           as: 'RolesUsuarios',
@@ -52,22 +53,44 @@ class PermisoRolService {
         idAplicacion: query.idAplicacion
       }
     });
+
     const mostrarData = [];
+    // let encontrar  = permisoUsuario.find(element => element.id ===  permisoUsuario.PermisosRoles.dataValues[0]);
+    // console.log({obj: encontrar});
+    // const mostrarData1 = await models.Permiso.findAll()
+
+    // const map = permisoUsuario.map(dat => ({ id: permisoUsuario[0]._previousDataValues.PermisosRoles[0].idPermiso }))
+
 
     permisoUsuario.forEach(data => {
-      mostrarData.push({
-
-        idRol:        `${data.idRol}`,
-        idAplicacion: `${data.idAplicacion}`,
-        idUsuario:    `${permisoUsuario[0]._previousDataValues.RolesUsuarios[0].idUsuario}`,
-        idPermiso:    `${permisoUsuario[0]._previousDataValues.PermisosRoles[0].idPermiso}`,
-        idPermisoRol: `${permisoUsuario[0]._previousDataValues.PermisosRoles[0].PermisoRol.idPermisoRol}`
+      // console.log({idRol: data.idRol});
+      // mostrarData.push({
+      // // idRol: `${data.idRol}`,
+      // })
+      data.PermisosRoles.forEach( dat => {
+        // console.log({PermisoRol:dat.idPermiso});
+        // console.log({IdPermisoRol: dat.PermisoRol.idPermisoRol});
+        mostrarData.push({
+          idRol: `${data.idRol}`,
+          idPermiso:  `${dat.idPermiso}`,
+          IdPermisoRol:      `${dat.PermisoRol.idPermisoRol}`,
+          })
+      
       })
+
+      // mostrarData.push({
+      //   idRol:        `${data.idRol}`,
+      //   idAplicacion: `${data.idAplicacion}`,
+      //   idUsuario:    `${data.RolesUsuarios[0].idUsuario}`,
+      //   idPermiso:    `${data.PermisosRoles[0].idPermiso}`,
+      //   idPermisoRol: `${data.PermisosRoles[0].PermisoRol.idPermisoRol}`
+      // })
+
     })
 
     return mostrarData;
   }
-
+  
   async findOne(id) {
     const permisoRol = await models.PermisoRol.findByPk(id);// buscar con id
     if (!permisoRol) {
