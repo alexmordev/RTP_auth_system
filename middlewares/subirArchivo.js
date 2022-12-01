daa5d74266d76e7221d59f6a4eb3fc8fe4c6d1d8
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-12-01 17:45:37 -0600

    Cambio error de imagen serchbranch

--- middlewares/subirArchivo.js
+++ middlewares/subirArchivo.js
@@ -1,20 +1,19 @@
 const path = require('path');
 
-const subirArchivo = ( file, carpeta, nombreAplicacion ) =>{
-    const extensionesValidas = ['png','jpg','jpeg']
+const subirArchivo = (file, carpeta, nombreAplicacion) => {
+    const extensionesValidas = ['png', 'jpg', 'jpeg']
     return new Promise((resolve, reject) => {
         const { imagen } = file;
- 
+
         const nombre = imagen.name.split('.')
-        const extension = nombre[ nombre.length -1 ];
-        
-        if( !extensionesValidas.includes(extension) ){
-            return reject(`La extension ${ extension} no es permitida, ${ extensionesValidas}`); 
+        const extension = nombre[nombre.length - 1];
+
+        if (!extensionesValidas.includes(extension)) {
+            return reject(`La extension ${extension} no es permitida, ${extensionesValidas}`);
         }
 
-        const nombreTemp = nombreAplicacion + '.' + extension ;
-        const uploadPath = path.join( __dirname, '../image/',carpeta, nombreTemp );
-          
+        const uploadPath = path.join(__dirname, '../image/', carpeta, nombreAplicacion);
+
         imagen.mv(uploadPath, (err) => {
             if (err) {
                 reject(err);
